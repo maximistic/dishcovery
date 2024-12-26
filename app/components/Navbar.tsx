b96f47abbd635346908e02885f7d44fd9fe14445
maximistic <110153672+maximistic@users.noreply.github.com>
2024-12-26 17:48:07 +0530

    navbar and layout configuration

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ThemeToggle } from './ThemeToggleButton'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div><ThemeToggle /></div>
+  )
+}
+
+export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "./context/theme-provider";
+import Navbar from "./components/Navbar";
 
 export const metadata: Metadata = {
   title: "DishCovery",
@@ -23,11 +15,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
