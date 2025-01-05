e686822fd6f18bed17dbaf68a862322b2ee82ac4
maximistic <110153672+maximistic@users.noreply.github.com>
2025-01-05 15:27:29 +0530

    config update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: 'standalone',  
   trailingSlash: true,   
   reactStrictMode: true,
+  images: {
+    domains: ['img.spoonacular.com'], 
+  },
 };
 
 export default nextConfig;
