c6cdbed5cc8604e991d64ecae08a4299648b83a2
maximistic <110153672+maximistic@users.noreply.github.com>
2025-01-06 13:39:31 +0530

    enhancements

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import React, { Suspense, useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { getRecipeByQuery } from "./api";
+import * as React from "react";
 import Recipe from "../components/SearchBody/Recipe";
 import Hero from "../components/SearchHero/Hero";
 
@@ -13,45 +11,12 @@ interface Recipe {
 }
 
 const SearchPageContent: React.FC = () => {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("query");
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      if (query) {
-        setLoading(true);
-        const results = await getRecipeByQuery(query);
-        setRecipes(results);
-        setLoading(false);
-      }
-    };
-
-    if (query) fetchRecipes();
-  }, [query]);
-
   return (
     <div>
       <div className="bg-white dark:bg-gray-900 py-0">
         <Hero />
       </div>
       <div className="container mx-auto px-4 py-8">
-        {/* <h1 className="text-3xl font-bold mb-4">
-          Search Results for {query || "No query"}
-        </h1>
-        {loading ? (
-          <p>Loading...</p>
-        ) : recipes.length === 0 ? (
-          <p>No recipes found.</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {recipes.map((recipe) => (
-              <SearchCard key={recipe.id} recipe={recipe} />
-            ))}
-          </div>
-        )} */}
-
         <Recipe />
       </div>
     </div>
@@ -60,9 +25,7 @@ const SearchPageContent: React.FC = () => {
 
 const SearchPage: React.FC = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
       <SearchPageContent />
-    </Suspense>
   );
 };
 
