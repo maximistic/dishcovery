b380bbfd75664aaa9b9d6aeaa9d2e768f274fea4
maximistic <110153672+maximistic@users.noreply.github.com>
2025-01-13 13:51:44 +0530

    homepage content

--- app/components/Home/Homepage.tsx
+++ app/components/Home/Homepage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import Cuisines from './Cuisines'
 
 function Homepage() {
   return (
-    <div>
+    <div className='bg-orange-100 w-full dark:bg-gray-800'>
       <Hero />
+      <Cuisines />
     </div>
   )
 }
