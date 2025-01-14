3f362935f78a401a8210bef0e07547664ce46e43
maximistic <110153672+maximistic@users.noreply.github.com>
2025-01-14 16:49:02 +0530

    api update

--- app/lib/Home.ts
+++ app/lib/Home.ts
@@ -13,17 +13,17 @@ export const fetchRecipesByCuisine = async (cuisine: string) => {
   return data.results;
 };
 
-// Fetch Recipes by Diet
-export const fetchRecipesByDiet = async (diet: string) => {
+
+export const fetchRecipesByMealType = async (mealType: string) => {
   const API_KEY = process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY;
-  const API_URL = `https://api.spoonacular.com/recipes/complexSearch?type=${diet}&number=4&apiKey=${API_KEY}`;
+  const API_URL = `https://api.spoonacular.com/recipes/complexSearch?mealType=${mealType}&number=4&fillIngredients=true&apiKey=${API_KEY}`;
 
   const response = await fetch(API_URL);
 
   if (!response.ok) {
-    throw new Error("Failed to fetch diets");
+    throw new Error("Failed to fetch recipes");
   }
 
   const data = await response.json();
   return data.results;
-};
+}
